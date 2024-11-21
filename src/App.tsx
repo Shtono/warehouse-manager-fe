a3b7ad221d85c42c25a8d4bd67e60af513a89757
Tsveto <tsvetomir.batinov@toggl.com>
2024-11-21 16:25:19 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Outlet } from 'react-router-dom'
 import AppBarComponent from './components/AppBarComponent/AppBarComponent.tsx'
 
 function App() {
+  console.log('App component rendered')
   return (
     <Stack width="100VW" height="100VH" gap={3}>
       <AppBarComponent />
